56a42241920e585a9d4ca982ef75f3a610dc3c73
Nick <niiickolay@gmail.com>
2018-09-20 16:24:41 +0300

    added touch event

--- js/script-2.js
+++ js/script-2.js
@@ -9,7 +9,6 @@ const buttonNext = document.querySelector(".slider__next");
 function Slider(slider) {
   this._slider = document.querySelector(slider);
   this._frame = this._slider.querySelector(".slider__frame");
-  // console.log(this._frame);
   this._prev = this._slider.querySelector(".slider__prev");
   this._next = this._slider.querySelector(".slider__next");
   this._path = 0;
@@ -32,35 +31,48 @@ Slider.prototype.showPrev = function() {
   this._frame.style.transform = `translateX(${this._path}px)`;
 }
 
-const s = new Slider(".slider");
 
 
-list.addEventListener("wheel", (event) => {
-  let delta = event.deltaY || event.detail || e.wheelDelta;
+// if (window.matchMedia("(min-width: 768px)").matches) {
+  const s = new Slider(".slider");
 
-  if (delta > 0) {
-    s.showNext();
-  }
+  list.addEventListener("wheel", (event) => {
+    let delta = event.deltaY || event.detail || e.wheelDelta;
 
-  if (delta < -0) {
+    if (delta > 0) {
+      s.showNext();
+    }
+
+    if (delta < -0) {
+      s.showPrev();
+    }
+
+    event.preventDefault();
+  });
+
+  buttonPrev.addEventListener("click", (event) => {
+    event.preventDefault();
+
     s.showPrev();
+  });
+
+  buttonNext.addEventListener("click", (event) => {
+    event.preventDefault();
+
+    s.showNext();
+  });
+
+  list.addEventListener("touchmove", handleMove);
+
+  function handleMove(event) {
+    event.preventDefault();
+    let touches = event.changedTouches;
+    // let firstTouch = event.changedTouches[0].screenX;
+    if (touches[0] - touches[touches.length - 1] > 0) {
+      s.showNext();
+    } else if (touches[0] - touches[touches.length - 1] < 0) {
+      s.showPrev();
+    }
+    console.log(touches);
   }
-
-  event.preventDefault();
-});
-
-buttonPrev.addEventListener("click", (event) => {
-  event.preventDefault();
-
-  s.showPrev();
-});
-
-buttonNext.addEventListener("click", (event) => {
-  event.preventDefault();
-
-  s.showNext();
-});
-
-list.addEventListener("touchmove", (event) => {
-  // console.log(event);
-})
+// }
